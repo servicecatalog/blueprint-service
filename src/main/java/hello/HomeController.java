6699da153a4e64ac9b969abf7051096ac60141d7
Michal <michal.grubski@ts.fujitsu.com>
2017-05-24 10:14:31 +0200

    Initial commit

---
+++ src/main/java/hello/HomeController.java
@@ -0,0 +1,17 @@
+package hello;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by PLGrubskiM on 2017-05-24.
+ */
+@Controller
+public class HomeController {
+
+    @RequestMapping("/")
+    public String index() {
+        return "index.html";
+    }
+
+}
