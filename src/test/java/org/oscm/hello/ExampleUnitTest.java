2b43538733c66980825ec919ad5c6f9f824f544a
Michał Grubski <michal.grubski@ts.fujitsu.com>
2017-05-26 12:47:56 +0200

    Docker (#14)
    * Added sample dockerfile and fabric8 plugin
    
    * Added docker build and push during the install maven goal
    
    * Removed duplicate entry after merge
    
    * Fixes #11. SpringRunner removed.

--- src/test/java/org/oscm/hello/ExampleUnitTest.java
+++ src/test/java/org/oscm/hello/ExampleUnitTest.java
@@ -6,18 +6,15 @@ package org.oscm.hello;
 
 import org.junit.Assert;
 import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.oscm.services.BlueprintService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
-import org.oscm.services.BlueprintService;
 
 /**
  * Created by PLGrubskiM on 2017-05-24.
  */
-@RunWith(SpringRunner.class)
 @WebAppConfiguration
 @WebMvcTest(BlueprintService.class)
 @ComponentScan({"org.oscm.services"})
